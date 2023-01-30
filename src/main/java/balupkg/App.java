fe3311f06ea8517ef4cb4b2a56b2fee5c6808d05
suresh-22 <120629370+suresh-22@users.noreply.github.com>
2023-01-30 12:03:55 +0530

    Add files via upload

---
+++ src/main/java/balupkg/App.java
@@ -0,0 +1,13 @@
+package balupkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
